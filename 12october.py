2d0b9e767b00204b1db19a8f971d975f1fa36524
bhanu-priyaa <devansh80771@gmail.com>
2022-10-12 19:20:32 +0530

    uploaded 12october.py

---
+++ 12october.py
@@ -0,0 +1,17 @@
+#arrays
+#a = int(0)
+# arr = [1,2,3,4,5]
+# a  = int(1)
+# b = int(2)
+# c = int(3)
+# d = int(4)
+# e = int(5)
+# print(a)
+# print(b)
+# print(c)
+# print(d)
+# print(e)
+# arr = [1,2,3,4,5]
+# print(arr[0])
+#array is a sequential collection of similar data
+#array is declared witht he help of square brackets
