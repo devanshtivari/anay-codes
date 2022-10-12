1a6ce9ca8a7912eebc4fafff691cbae47990a746
devanshtivari <devansh80771@gmail.com>
2022-10-12 19:13:54 +0530

    Add files via upload

---
+++ 11october.py
@@ -0,0 +1,47 @@
+#looping
+#instructing the computer to performm a specific action more than once is called as looping.
+#circle
+#loops are of two types: 1) for loop 2)while loop
+#for loop : beginning and the ending predefined
+#while loop : ending is predefined
+#arrays, lists, tuples
+#program to write first 10 natural numbers
+#1..10
+# for i in range(10):
+#     print(i)
+
+#range: start:1 end:10-1 = 9
+#range(parameters) : start , end , increment
+#in : contained
+# for i in range(1,11):
+#     print(i)
+#print the summ of first 10 natural numbers 
+#0 - infinity
+# sum = int(0)
+# for i in range(1,11):
+#     sum = sum+i
+# print(sum)
+#factorial : 5! : 5x4x3x2x1 = 120
+#6! : 6x5x4x3x2x1 = 720
+# fact = int(1)
+# for i in range(1,6):
+#     fact = fact*i
+# print(fact)
+#fact = 1
+#1 , 2 , 3 , 4 , 5 = i
+#fact = 1 , 2 , 6 , 24 , 120
+#HW : take the input from the user and print the sum till that integer and the factorial till that integer 
+#enter number : 5
+#sum : 1+2+3+4+5 : 15
+#factorial : 1x2x3x4x5 : 120
+#while loop 
+#while(condition): if the condition is true then you will enter the loop and if the condition in false you will come out of the loop/
+# i = int(20)
+# while(i>10):
+#     print(i)
+#     i = i-1
+# for i in range(20,10,-1):
+#     print(i)
+#looping need of looping concept of looping 
+#types of looping 
+#discussion and detailed implementations
---
+++ 8october.py
@@ -0,0 +1,64 @@
+#.exe .py .cpp .java .msi .txt
+#javascript python c++ java
+#ai ml web app cp dsa
+#1980: b and c c++ : operating systems: windows mac linux
+#java: oops app os python
+#guido van russum : sun microsystem
+#easy to write easy to implement human readable 
+#monty python ->python
+#python : oops , easy to learn , easy to implement , libraries and built in modules
+#books : superpowers 
+#machine learning : 
+#web devlopment : 
+#printing the hello world
+#print("hello world")
+##include<iostream>
+#using namespace std;
+#int main() {
+# cout<<"hello world";}
+#import java.util.*;
+#class main() {
+# public static void main(String[] args){
+# System.out.println("hello world");
+# }
+# }
+#revision of python
+#printing : print()
+#input the data : input()
+#int string float 
+#matter : solid liquid gas 
+#container : box bottle cylinder
+#data : number alphabet decimel
+#container : int string float
+#dataypes: containers used to store data 
+#RAM : Random access memory 
+#10 -> int 
+#anay -> string
+#10.06 -> float
+#a is a string
+#by default every thing is considered as string
+#conditional statments
+#if else elif
+#determine whether a person is eligible to vote or not
+#age is above 18 years, then he/she is eligible to vote.
+# i = int(input("Enter you age : "))
+# if(i >= 18):
+#     print("You are eligible to vote")
+# else:
+#     print("You are not eligible to vote")
+#if statments
+#if else statements
+#if elif else statements
+#if your marks are above 80 then your grade is A, if your marks are between 80-60 then your grade is B otherwise your grade is C.
+#marks<80 -> A
+# marks>80 and marks<60 -> B
+# marks<60 -> C
+# a = int(input("Enter your marks : "))
+# if(a>=80):
+#     print("Your grade is A")
+# elif a<80 and a>=60:
+#     print("Your grade is B")
+# else:
+#     print("Your grade is C")
+#margin values : 80 and 60 
+#revision is done
