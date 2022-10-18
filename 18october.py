17126be553768fe9b35059f5cd415a22b4601c6a
bhanu-priyaa <devansh80771@gmail.com>
2022-10-18 21:39:42 +0530

    added file 18october

---
+++ 18october.py
@@ -0,0 +1,44 @@
+#lists and its functions
+#arrays
+# list = [2 , 3 , "anay" , 23.03 ]
+# print(list)
+#functions are predefined codes which are there to perform specific tasks
+# a = int(18)
+# b = int(12)
+# c = a+b
+#program to add 2+3 34+45 23+33 10+23 1+9
+# def sum(a,b):
+#     c =  a+b
+#     return c
+
+# print(sum(2,3))
+# print(sum(34,45))
+#program to multiply two numbers 2x3 43x2 34x32
+# def multiply(a,b):
+#     c = a*b
+#     return c
+# print(multiply(2,3))
+# print(multiply(43,2))
+# print(multiply(34,32))
+
+#functions related to lists/arrays
+#append() : is used to insert elements to the last of the list
+#[2,3,4,5]
+#append(6)
+#[2,3,4,5,6]
+#insert() : is used to insert element at a specific position
+#[2,3,4,5,6]
+#insert(1,2) 1st parameter: index |  2nd parameter: value
+#[2,3,1,4,5,6]
+#extend() : is used to insert multiple items into the list
+#[2,3,4,5,6]
+#extend(1,10,11)
+#[2,3,4,5,6,1,10,11]
+
+# li = [2,3,4,5,6,7]
+# print("the list before append function : ",li)
+# li.extend([8,9,10])
+# print("the list after append function : ",li)
+
+#HW: write python code to implement calculator using functions 
+#HW: create a list and implement append, insert and extend function
