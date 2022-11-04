c0707411ed67ad881e9dd46d0da180db6e3a6ff4
bhanu-priyaa <devansh80771@gmail.com>
2022-11-04 10:26:23 +0530

    dictionaries , compiler and files

---
+++ 3november.py
@@ -0,0 +1,47 @@
+#dictionaries
+#word -> meaning [real life]
+#key -> value [python]
+#anay : xxxx12424
+#amit : xxxx12422
+#key should be unique
+#key can be of any length
+# anay = {
+#     "name" : "anay",
+#     "age" : "13",
+#     "country" : "switzerland",
+#     "phone" : "112343"
+# }
+
+# john = {
+#     "name": "john",
+#     "age": "18",
+#     "country": "india",
+#     "phone": "11234"
+# }
+# #list : [] squares
+# #arrays , sets : () round
+# #dictionaries : {} curly brackets
+
+# print(john.get("age"))
+
+#difference between a compiler and an interpreter
+#compiler : at once
+#interpreter : line by line
+#compiler is a software which compiles the code all at once
+#interpreter compiles the code line by line 
+#.py : english 
+#.exe : machine language
+#.cpp , .java , .js  -> .exe 
+#python , javascript : interpreter 
+#c++ , java : compiler
+# print("Start of the program")
+# print("in the middle of the program")
+# a = input("Enter a number")
+#compiler compiles the code all at once
+#interpreter compiles the code line by line
+
+#modules : inbuilt codes which can increase the effeciency of your program
+#how to crea
+
+
+
