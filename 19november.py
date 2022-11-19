2fb50b0b9219fb810f501dda292c43eb6a8c3a8b
bhanu-priyaa <devansh80771@gmail.com>
2022-11-19 22:56:28 +0530

    operators, looping statments

---
+++ 19november.py
@@ -0,0 +1,48 @@
+#switch statments
+# def Add(a,b):
+#     return a + b
+# def Sub(a,b):
+#     return a - b
+# def Multiply(a,b):
+#     return a * b
+# def Divide(a,b):
+#     return a / b
+
+# a = int(input("Enter the first number"))
+# b = int(input("Enter the second number"))
+# choice = int(input("Enter your choice 1.Addition 2.Substraction 3.Multiplication 4.Division"))
+
+#operators 
+# a + b
+#a,b are operands
+#+ is the operator
+#a%2 == 0 then even
+#a,2,0 are operands
+#% , == are operators
+
+# a+b-c == 2
+# + - == are operators
+# a b c 2 are operands
+
+
+#types of operators : 
+# 1. Arithematic operator: Arithmatic calculations
+#    for ex: + , - , * , / , % , // , ^ 
+# 2. Relational operator: Used for relations
+#    for ex: == , != , <= , >= 
+# 3. Assignment operator: Assigning a value 
+#    for ex: =
+# 4. Bitwise operators: bit manipulation
+# 0 or 1
+# 8 bits = 1 byte 
+# 1024 bytes = 1 kilobyte {kB}
+#1024 kilobytes = 1 megabyte {mB}
+#1024 megabytes = 1 gigabytes {gB}
+#1024 gigabytes = 1 terabyte{tB}
+# 4 number systems
+# 1. binary number system : 0,1
+# 2. decimel number system : 0-9
+# 3. octal number system: 0-7
+# 4. Hexadecimel number system: 0-9 A-F 
+
+
