1ada320638d6201d6068205aa8fa03b423576b93
bhanu-priyaa <devansh80771@gmail.com>
2023-02-02 22:48:17 +0530

    added files

---
+++ 2february.py
@@ -0,0 +1,25 @@
+#datatypes
+#string: text alphabets,sentences
+#integer: number
+#float: decimel numbers
+#boolean: true false
+#lists,tuples,sets,arrays: collection of data
+#dictionaries: key-value pairs 
+#conditional statement
+#if elif else
+#voter person age above 18 years then he/she is eligible to vote
+#if(age>=18):
+# print("eligible to vote")
+#loops
+#for,while loops
+#program to write first 10 numbers: for loop and while loop
+# for i in range(1,11):
+#     print(i)
+# i = int(1)
+# while(i<11):
+#     print(i)
+#     i = i+1
+#for loop works on the basis of range
+#while loop works on the basis of condition
+#in while loop we have a greater chance of infinite loop
+#functions
