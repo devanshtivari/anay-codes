8e082bab4b29b1935daa573eff40b49c2e46a0e5
bhanu-priyaa <devansh80771@gmail.com>
2023-03-03 22:50:05 +0530

    implemented binary search

---
+++ 3march.py
@@ -0,0 +1,42 @@
+#binary search
+#divide and conquer algorithm: Divding the big problem into sub problem and finding solutions
+#problem : searching a particular element
+#brute force algorithm: linear search
+#        1 2 3 4 5 6 7 8 
+# arr = [1,2,3,4,5,6,7,8]
+# key = int(11) #element which we have to find out #6
+# flag = int(0) #indicator to find if the element exists or not #0
+# left = int(0) #0  #4
+# right = int(7) #7
+# while(left != right):
+#     mid = int((left+right)/2) #3   #5
+#     if arr[mid] == key:
+#         flag = 1
+#         break
+#     elif(arr[mid]<key):
+#         left = mid+1
+#     elif(arr[mid]>key):
+#         right = mid-1
+
+# if flag == 1:
+#     print("element found")
+# else:
+#     print("element not found")
+
+#searching algorithms
+#linear search: going to each ad every element in a list {collection} to find the element
+#binary search: divide by two over and over again
+
+#binary search is always used in case of sorted arrays
+#sort that array and then use binary search 
+
+
+#sorting in python
+#bubble sort
+#quick sort
+#merge sort
+
+#homework: implement binary and linear search. 
+#array of numbers, array of names in which you can find through first alphabet
+#[1,2,3,4,5,6,7,8]
+#["anay" , "bs" , "patrick" , "susan"]
